5e85f2bd0cd380e9fcc2ca3acf38c75b3a597542
Leonardo Souza <leonardosilva.souza@yahoo.com.br>
2018-04-18 11:35:58 -0300

    Create iam_custom_role.js

---
+++ iam_custom_role.js
@@ -0,0 +1,28 @@
+# First: Create IAM Custom Role having the following code:
+# This is JSON file
+
+#Javascritp
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:DescribeInstances",
+        "ec2:DescribeRegions",
+        "ec2:StartInstances",
+        "ec2:StopInstances"
+      ],   
+      "Resource": "*"
+    }	
+  ]
+}

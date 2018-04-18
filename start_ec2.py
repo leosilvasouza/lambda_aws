066edf5cec347fe97095d89a60f844f5c5755c8d
Leonardo Souza <leonardosilva.souza@yahoo.com.br>
2018-04-18 13:01:39 -0300

    Create start_ec2.py
    ec2 Instance having ScheduledStartStop tag like true it will call the procedure.

---
+++ start_ec2.py
@@ -0,0 +1,40 @@
+import boto3
+import sys, traceback
+from datetime import datetime
+from time import sleep
+
+def start_ec2_instances():
+    start_time = datetime.now()
+
+    # starting ec2 client
+    ec2_client = boto3.client('ec2')
+
+    regions = ec2_client.describe_regions()
+
+    for region in regions['Regions']:
+        try:
+            print("Region: " + str(region['RegionName']))
+            ec2_client = boto3.client('ec2', region_name=region['RegionName'])
+            instances = ec2_client.describe_instances()
+            instanceIds = list()
+            
+            for reservation in instances['Reservations']:
+                for instance in reservation['Instances']:
+                    if instance['State']['Name'] == "stopped" and not instance['Tags'] is None : 
+                        for tag in instance['Tags']:
+                            try:
+                                if tag['Key'] == 'ScheduledStartStop' and tag['Value'] == 'True'    :
+                                    instanceIds.append(instance['InstanceId'])
+                            except:
+                                print "Not expected error: ", traceback.print_exc()
+                      
+            if len(instanceIds) > 0 : 
+                print "Starting instances: " + str(instanceIds)
+                ec2_client.start_instances(InstanceIds=instanceIds)                                                   
+                                                            
+        except:
+            print "Not expected error:", traceback.print_exc()
+                                                           
+    end_time = datetime.now()
+    took_time = end_time - start_time
+    print "Total time of execution: " + str(took_time)
